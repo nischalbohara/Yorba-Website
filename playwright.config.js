e3fab47e4f6a411e97ce0f2b8862b01994f5f332
nischalbohara <nischalbohara2012@hotmail.com>
2026-09-14 10:27:10 +0545

    Workers =5

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Limit workers on CI for stability; unrestricted locally */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
